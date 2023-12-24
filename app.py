1a24829cb1c64012557ecc5b7eaeaae6e2dd3ed1
itsthomashere <thomasadam.leigh@gmail.com>
2023-12-24 12:23:37 +0200

    Add app.py and pyproject.toml files

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit-option-menu import option_menu
+
+from sqlalchemy import create_engine, text
+import pandas as pd
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import QAGenerationChain
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+
+st.title("Research Assistant 2.0")
+
+user_input = st.chat_input("Send a message...", key="user_input")
